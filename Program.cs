836fd0cbd6a1deba080397aa663428be8b954bac
LuanGCMenezes <luangui2006@gmail.com>
2025-04-05 16:13:50 -0300

    Adicionando Exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+double tempoDeDiario, consumoDoAparelho, custoEnergia, custoDeUmMes;
+
+Console.WriteLine("--- Custo de Energia ---");
+
+Console.Write("Consumo do aparelho (em KWh/mês)...:");
+consumoDoAparelho = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Horas de uso por dia...............:");
+tempoDeDiario = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Custo de energia (em R$/KWh).......:");
+custoEnergia = Convert.ToDouble(Console.ReadLine()!);
+
+
+custoDeUmMes = tempoDeDiario * consumoDoAparelho * custoEnergia;
+
+Console.Write("Custo estimado do mês será.........:");
+Console.WriteLine($"R$ {custoDeUmMes:F2}");
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# CustoEnergia
 Um aparelho Ar Condicionado Split LG DUAL Inverter 9.000 Frio consome 17,1 kWh/mês de energia enquanto ligado,
